4085fc1a757f9e4d576542b6605d4a2b02f587e1
hitesh kolhe <hiteshkohle25gmail.com>
2025-01-31 16:54:04 +0530

    backend basic structure added

---
+++ OSMS_Backend_template/src/main/java/com/osms/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.osms;
+
+import org.modelmapper.Conditions;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper mapper=new ModelMapper();
+		mapper.getConfiguration()
+		//data xfer from src props -> dest props iff 
+		//->1.  names n data types match
+		.setMatchingStrategy(MatchingStrategies.STRICT)
+		// && 2, src property is not null 
+		.setPropertyCondition(Conditions.isNotNull());
+		return mapper;
+	}
+}
